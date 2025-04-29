10c157b0182ea7cf48ab5aadee93cab47ac1cf15
interesting12 <giorgishavadze09@gmail.com>
2025-04-30 00:53:16 +0400

    second commit

---
+++ level 143/classwork/vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import { Index } from "./components/home"
+function App() {
+  return (
+    <>
+      <Index />
+      <h1 className=' text-cyan-600'>Hello Word!</h1>
+    </>
+  )
+}
+
+export default App
+
+
---
+++ level 143/classwork/vite-project/src/components/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Index = () => {
+  return (
+    <div>
+        <h1 className=' text-amber-600 hover:text-amber-200 cursor-pointer flex justify-center items-center h-100'>Hello</h1>
+    </div>
+  )
+}
+
